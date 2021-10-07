40bd6a6ff795ab47d048129d02fe334e72b76a97
Yana <dvoryansakaya.yana@gmail.com>
2021-10-07 17:51:00 +0300

    change player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,38 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody _rb;
-    [SerializeField] private float _jumpForce = 1f;
-    private int jumpCount = 0;
-    private int extraJumps;
-    private int extraJumpsValue;
-    private bool isGrounded;
+    private float jumpSpeed = 5f;
+    private Rigidbody rigidbody;
+    private bool onGround;
 
-
-    private void Start()
+    void Start()
     {
-        extraJumps = extraJumpsValue;
-        _rb = GetComponent<Rigidbody>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-
-
-    private void Update()
-    {
-        if (isGrounded == true)
-        {
-            extraJumps = extraJumpsValue;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && extraJumps <= 1)
-        {
-            _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-            jumpCount++;
-        } 
-
-    }
-
+    
 }
