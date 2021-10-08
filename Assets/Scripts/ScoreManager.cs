9e1250f86235271b6ace4b312198bac1ed532f05
Shilaev Valentin <valentin.shilaev@yandex.ru>
2021-10-08 11:05:35 +0300

    + refactor Score manager

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,19 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    private float _currentScore;
-
-    public float CurrentScore
-    {
-        get => _currentScore;
-        private set => _currentScore = value;
-    }
+    public float CurrentScore { get; private set; }
 
     /// <summary>
-    /// Add given number of score to current score
+    ///     Add given number of score to current score
     /// </summary>
     /// <param name="score"></param>
     public void AddScore(float score)
